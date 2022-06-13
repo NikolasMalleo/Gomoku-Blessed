8a71b213ba35d51e32e5dd166d0ab6bc7249757d
NikolasMalleo <NikolasMalleo@gmail.com>
2022-06-13 20:36:08 +0300

    trash

--- index.js
+++ index.js
@@ -1,71 +1,33 @@
-var blessed = require('blessed');
+var blessed = require('blessed')
+  , program = blessed.program();
  
-// Create a screen object.
-var screen = blessed.screen({
-  smartCSR: true
+program.key('q', function(ch, key) {
+  program.clear();
+  program.disableMouse();
+  program.showCursor();
+  program.normalBuffer();
+  process.exit(0);
 });
  
-screen.title = 'my window title';
- 
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.box({
-  top: 'center',
-  left: 'center',
-  width: '50%',
-  height: '50%',
-  content: 'Hello {bold}world{/bold}!',
-  tags: true,
-  border: {
-    type: 'line'
-  },
-  style: {
-    fg: 'white',
-    bg: 'magenta',
-    border: {
-      fg: '#f0f0f0'
-    },
-    hover: {
-      bg: 'green'
-    }
+program.on('mouse', function(data) {
+  if (data.action === 'mousemove') {
+    program.move(data.x, data.y);
+    program.bg('red');
+    program.write('x');
+    program.bg('!red');
   }
 });
  
-// Append our box to the screen.
-screen.append(box);
+program.alternateBuffer();
+program.enableMouse();
+program.hideCursor();
+program.clear();
  
-// Add a png icon to the box
-var icon = blessed.image({
-  parent: box,
-  top: 0,
-  left: 0,
-  type: 'overlay',
-  width: 'shrink',
-  height: 'shrink',
-  file: __dirname + '/my-program-icon.png',
-  search: false
-});
- 
-// If our box is clicked, change the content.
-box.on('click', function(data) {
-  box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-  screen.render();
-});
- 
-// If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
-  box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-  box.setLine(1, 'bar');
-  box.insertLine(1, 'foo');
-  screen.render();
-});
- 
-// Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
- 
-// Focus our element.
-box.focus();
- 
-// Render the screen.
-screen.render();
+program.move(1, 1);
+program.bg('black');
+program.write('Hello world', 'blue fg');
+program.setx((program.cols / 2 | 0) - 4);
+program.down(5);
+program.write('Hi again!');
+program.bg('!black');
+program.feed();
