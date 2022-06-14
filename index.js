f8d678c867ee039330e1c6a6cb66e6d366792728
NikolasMalleo <NikolasMalleo@gmail.com>
2022-06-14 14:39:56 +0300

    create main list

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
 var blessed = require('blessed')
   , program = blessed.program();
- 
-program.key('q', function(ch, key) {
-  program.clear();
-  program.disableMouse();
-  program.showCursor();
-  program.normalBuffer();
-  process.exit(0);
-});
- 
-program.on('mouse', function(data) {
-  if (data.action === 'mousemove') {
-    program.move(data.x, data.y);
-    program.bg('red');
-    program.write('x');
-    program.bg('!red');
+
+process.title = 'blessed';
+
+x = 0
+y = 0
+let GameBoard = []
+
+
+program.on('keypress', function(data, key) {
+  if (key.name === 'q') {
+    program.clear();
+    program.disableMouse();
+    program.showCursor();
+    program.normalBuffer();
+    process.exit(0);
+  }
+  if (key.name === 'c') {
+    program.write(x + " " + y)
+  }
+  if (key.name === 'w') {
+    if (y >= 0){
+      y = y - 1
+    }
+    else {
+      y = 0
+    }
+    program.move(x, y)
+  }
+  if (key.name === 's') {
+    if (y >= 0){
+    y = y + 1
+    }
+    else {
+      y = 0
+    }
+    program.move(x, y)
+  }
+  if (key.name === 'a') {
+    if (x >= 0){
+    x = x - 1
+    }
+    else {
+      x = 0
+    }
+    program.move(x, y)
+  }
+  if (key.name === 'd') {
+    if (x >= 0){
+    x = x + 1
+    }
+    else {
+      x = 0
+    }
+    program.move(x, y)
   }
 });
- 
-program.alternateBuffer();
-program.enableMouse();
-program.hideCursor();
-program.clear();
- 
-program.move(1, 1);
-program.bg('black');
-program.write('Hello world', 'blue fg');
-program.setx((program.cols / 2 | 0) - 4);
-program.down(5);
-program.write('Hi again!');
-program.bg('!black');
-program.feed();
+
+function makeMainList(){
+  for (let x = 0; x < 15; x++) {
+    let tempArray = [];
+    for (let y = 0; y < 15; y++) {
+      if (y === 16) {
+        tempArray.push('\n');
+      } else {
+      tempArray.push(' ');
+     }
+    }
+    GameBoard.push(tempArray);
+  }
+ };
+
+ makeMainList()
+ program.clear();
+ console.log(GameBoard[0].length);
+
